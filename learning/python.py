db691c5172d0f7f096427ff674ac11228e7a861b
shaghaygh <shqayqm145@gmail.com>
2024-06-12 12:50:53 +0330

    12/6/2024

---
+++ learning/python.py
@@ -0,0 +1,26 @@
+# print('hello world');
+
+# print(type(2));
+# print(type(2.4));
+# print(type(2j));
+# print(type('fff'));
+# print('----------------');
+# print(3 + 6);
+# print(3 + 6.5);
+# print(type(-2));
+
+# print(3 ** 2);
+# print(9 ** 0.5);
+# print(9 % 2);
+# print(68 // 6);  # print((68 - (68 % 6)) / 6);
+
+x , y = 100 , 50;
+print(x + y);
+myNum = 70;
+myNum = myNum - 10 ;
+print(myNum + y);
+productName = "python course";
+productPrice = "100$"
+productTag = ['a' , 'b' , 'c' , 'd' , 6]  # list not array
+product = {"productName" : "python course" , "productPrice" : "100$"}
+print(product);
