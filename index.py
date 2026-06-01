ec60e40e08c48e289e33641415a3e004c9354f8e
shaghaygh <shqayqm145@gmail.com>
2026-06-01 22:48:57 +0330

    start learning

---
+++ index.py
@@ -0,0 +1,21 @@
+# print('hello')
+# print('Shaghayegh')
+# print(type('sh'))
+# print(type(5))
+# print(type(5.5))
+# print(type(5j))
+
+# print('-----------------------------')
+# print(3+6)
+# print(3+6.0)
+# print(3+6.4)
+# print(5-3.4)
+# print(2-3.4)
+# print(5.4-2)
+# print(49/7)
+# print(49/7+2*9)
+
+
+
+
+
